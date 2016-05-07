1955ab3e696462bd787bbb120e1182cdff66db5d
tanraya <demerest@gmail.com>
2016-05-08 03:45:25 +0500

    Update entity map

--- src/app/entity_map.js
+++ src/app/entity_map.js
@@ -1,17 +1,21 @@
-import ParagraphEntity from './entities/paragraph.js';
-import ImageEntity from './entities/image.js';
-import TableEntity from './entities/table.js';
+import EmbedEntity from './entities/embed.js';
 import GridEntity from './entities/grid.js';
 import GridColumnEntity from './entities/grid_column.js';
-import SubstrateEntity from './entities/substrate.js';
+import ImageEntity from './entities/image.js';
+import ListEntity from './entities/list.js';
+import ParagraphEntity from './entities/paragraph.js';
 import RootContainerEntity from './entities/root_container.js';
+import SubstrateEntity from './entities/substrate.js';
+import TableEntity from './entities/table.js';
 
 export default {
-  Paragraph     : ParagraphEntity,
-  Image         : ImageEntity,
+  Embed         : EmbedEntity,
   Grid          : GridEntity,
-  Substrate     : SubstrateEntity,
-  RootContainer : RootContainerEntity,
   GridColumn    : GridColumnEntity,
+  Image         : ImageEntity,
+  List          : ListEntity,
+  Paragraph     : ParagraphEntity,
+  RootContainer : RootContainerEntity,
+  Substrate     : SubstrateEntity,
   Table         : TableEntity
 };
